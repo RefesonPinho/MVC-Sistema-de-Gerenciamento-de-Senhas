3cfde2e88211f9c1d35d4e9f6a6f70260c583ea7
RefesonPinho <refeson_automacao@outlook.com>
2022-04-10 17:15:50 -0300

    Criando Crontroller Tag

---
+++ ProjetoFinal/Controllers/Tag.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Models;
+using System.Text.RegularExpressions;
+
+namespace Controllers
+{
+    public class TagController 
+    {
+        public static Tag InserirTag(
+            string Descricao
+        )
+        {
+            if (String.IsNullOrEmpty(Descricao))
+            {
+                throw new Exception("Nome inválido");
+            }
+           
+
+            return new Tag(Descricao);
+        }
+
+        public static Tag AlterarTag(
+            int Id,
+            string Descricao
+        )
+        {
+            Tag Tag = GetTag(Id);
+            
+            string altDescricao = !String.IsNullOrEmpty(Descricao) ? Descricao : Tag.Descricao ;
+
+            Tag.AlterarTag(Id, altDescricao);
+
+            return Tag;
+        }
+
+        public static Tag ExcluirTag(
+            int Id
+        )
+        {
+            Tag Tag = GetTag(Id);
+            Tag.RemoverTag(Tag);
+            return Tag;
+        }
+
+        public static IEnumerable<Tag> GetTags()
+        {
+            return Tag.GetTags();
+        }
+
+        public static Tag GetTag(int Id)
+        {
+            Tag tag = (
+                from Tag in Tag.GetTags()
+                    where Tag.Id == Id
+                    select Tag
+            ).First();
+
+            if (tag == null)
+            {
+                throw new Exception("Tag não encontrado");
+            }
+
+            return tag;
+        }
+    }
+}
---
+++ ProjetoFinal/Program.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Windows.Forms;
+
+
+namespace EncryptMe
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            /*Application.EnableVisualStyles();
+            Application.Run(new Login());*/
+            Console.WriteLine("Hellow");
+        }
+    }
+}
